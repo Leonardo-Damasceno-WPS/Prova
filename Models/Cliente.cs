03f4d63679275ba78381a4ae073fb46a9add4b2e
Leonardo-Damasceno-WPS <130994439+Leonardo-Damasceno-WPS@users.noreply.github.com>
2023-09-26 20:52:18 -0300

    Leonardo Damasceno EX19

---
+++ Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prova.Models
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string telefone { get; set; }
+        public string cep { get; set; }
+        public byte[] foto { get; set; }
+        public int status { get; set; }
+    }
+}
